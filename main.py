0c90a97d24682f25e62e72f7b0431b3dfcde549b
Joao-Adegas <pedrojoao6378@gmail.com>
2025-07-03 21:33:15 -0300

    Adicionada Funcionalidade do usuário poder digitar a url das duas formas

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
-from langchain_community.document_loaders import WebBaseLoader # Carrega o conteúdo de uma página da web.
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_groq import ChatGroq
 from langchain.prompts import ChatPromptTemplate
 
 from fastapi import FastAPI
-
 from model import InputData
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 app = FastAPI()
-api_key = os.getenv("api_key")
+api_key = os.getenv("GROQ_API_KEY")
 os.environ['GROQ_API_KEY'] = api_key
 chat = ChatGroq(model="llama-3.3-70b-versatile")
 
+def formatar_url(url):
+    url = url.strip()
+    # Apenas imprime no console para você conferir
+    print(f"URL original recebida: {url}")
+    
+    # Não altera nada se já for http(s)
+    if url.startswith("http://") or url.startswith("https://"):
+        return url.rstrip("/")
+    
+    # Caso não tenha o prefixo, adiciona https
+    return f"https://{url}".rstrip("/")
+
 
 @app.post("/")
-def pesquisar_no_site(dados:InputData):
+def pesquisar_no_site(dados: InputData):
+    url_formatada = formatar_url(dados.url)
+    print(f"URL final usada no Loader: {url_formatada}")
+    loader = WebBaseLoader(url_formatada)
 
-    
-    loader = WebBaseLoader(f"https://{dados.url}/") # passando a URL do site que deve ser analisado
-   
-    lista_documentos = loader.load() # carregando o conteudo da página que foi passada
+    lista_documentos = loader.load()
 
-    # concatenando o conteúdo das páginas carregadas em uma única string 
-    documento = ''
-    for doc in lista_documentos:
-        documento = documento + doc.page_content
+    documento = ''.join(doc.page_content for doc in lista_documentos)
 
     template = ChatPromptTemplate.from_messages([
-        ("system","Você é um assistente amigável que tem acesso as seguintes informações para dar suas respostas.: {documentos_informados}. Tente dar as informações de forma resumida, e tente ser mais direto ao ponto)"),
-        ("user",'{input}')
+        ("system", "Você é um assistente amigável que tem acesso às seguintes informações: {documentos_informados}. Tente dar as informações de forma resumida, e tente ser mais direto ao ponto, dê a resposta no mínimo de linhas possíveis e apenas forneça detalhes se o usuário pedir."),
+        ("user", "{input}")
     ])
 
     chain = template | chat
-    resposta = chain.invoke({"documentos_informados":documento,"input":f"{dados.prompt}"})
-    # print(resposta.content)
+    resposta = chain.invoke({"documentos_informados": documento, "input": dados.prompt})
+
     return resposta.content
-  
